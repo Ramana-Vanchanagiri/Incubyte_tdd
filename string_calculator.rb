3d89d975357950a2fb795c16617d352388ecd013
Ramana Vanchanagiri <ramanavanchanagiri@Ramanas-MacBook-Air.local>
2025-07-13 23:38:32 +0530

    feat: support custom delimiters using //<delimiter>\n format

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,7 +1,16 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    num_arr = numbers.split(/,|\n/).map(&:to_i)
+
+    delimiter = /,|\n/
+
+    if numbers.start_with?("//")
+      header, numbers = numbers.split("\n", 2)
+      custom_delimiter = header[2..]
+      delimiter = Regexp.escape(custom_delimiter)
+    end
+
+    num_arr = numbers.split(/#{delimiter}|\n/).map(&:to_i)
     num_arr.sum
   end
 end
